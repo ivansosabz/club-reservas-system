104c2875c86c5257bb390f139341c5bcb3181c77
ivansosabz <ivansosabz@gmail.com>
2026-04-14 16:07:07 -0300

    feat: agregegado de los modelos Resource y ResourceTypes

--- backend/resources/admin.py
+++ backend/resources/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from resources.models import Resource, ResourceTypes
+
+admin.site.register(Resource)
+admin.site.register(ResourceTypes)
--- backend/resources/models.py
+++ backend/resources/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class ResourceTypes(models.Model):
+    name =models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=100)
+    resource_types = models.ForeignKey(
+        ResourceTypes,
+        on_delete=models.PROTECT, #si un tipo se esta usando no se eliminará
+        related_name='resources',
+    )
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
